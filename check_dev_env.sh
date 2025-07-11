436b3a83d8bb5d34eae43f8b36cf88d0d08017d1
Jalen Mangantulao <jalen.mangantulao@gmail.com>
2025-07-10 20:14:58 -0400

    update environment script

--- check_dev_env.sh
+++ check_dev_env.sh
@@ -1,17 +1,44 @@
 #!/bin/bash
 
-echo "Checking if a compatible Python version is installed..."
+echo -e "\e[35mChecking if a compatible Python version is installed..."
 python_ver=$(python3 --version)
+compatible_pyver="Python 3.10.X | Python 3.13.X"
 case "$python_ver" in
     "Python 3.10."*|"Python 3.13."*)
-        echo "Compatible Python version found - ("$python_ver")"
+        echo -e "\e[32m\u2714\e[0m Compatible Python version found - ("$python_ver")"
         ;;
     *)
-        echo "No compatible Python version found."
+        echo -e "\u274c\ No compatible Python version found. Please install a compatible Python version - ("$compatible_pyver")"
+        exit 1
         ;;
 esac
 
-requirements_pckgs=$(grep -oE '(\w+[^\=])+(\d?)' requirements.txt)
-user_pip_pckgs=$(pip list | grep -E "(\w.+)(\d+)")
+echo -e "\e[35mChecking if current user has required Python packages installed...\e[39m"
+requirements_pckgs=$(cut -d"=" -f1,3 --output-delimiter=" " requirements.txt > proj_temp.txt)
+user_pip_pckgs=$(pip list | tail -n +3 | tr -s " " > user_temp.txt)
 
-echo "$requirements_pckgs"
+awk '
+  NR==FNR {
+    key = tolower($1) " " $2
+    seen[key] = 1
+    next
+  }
+  {
+    key = tolower($1) " " $2
+    if (!(key in seen)) {
+      print "❌ Missing:", $0
+      missing_count = 1
+    }
+  }
+  END { 
+    if (!missing_count)
+        print "✅ All packages from requirements.txt are found in user installed Python packages"
+    else
+        printf "\nDo you want to install the missing Python packages? [Y/n] "
+        getline pkgresp < "/dev/stdin"
+        if (index(tolower($0), tolower(pkgresp)) == "y")
+            print "Installing missing (missing_count) packages from requirements.txt"
+            system("pip install -r requirements.txt")
+  }
+' user_temp.txt proj_temp.txt
+rm user_temp.txt && rm proj_temp.txt
